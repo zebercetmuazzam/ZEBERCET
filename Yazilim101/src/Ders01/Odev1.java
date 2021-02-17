31ff3084e8f9099bb385656feec62217e301b3d6
zebercetmuazzam <78850268+zebercetmuazzam@users.noreply.github.com>
2021-02-17 17:08:58 +0300

    alistirmalar

--- Yazilim101/src/Ders01/Odev1.java
+++ Yazilim101/src/Ders01/Odev1.java
@@ -1,48 +1,17 @@
 package Ders01;
 
-import java.util.Scanner;
 
 public class Odev1 {
 
 	public static void main(String[] args) {
-		   Scanner reader = new Scanner(System.in);
-		    System.out.print("Enter two numbers: ");
-
-		    double first = reader.nextDouble();
-		    double second = reader.nextDouble();
-
-		    System.out.print("Enter an operator (+, -, *, /): ");
-		    char operator = reader.next().charAt(0);
-
-		    double result;
-
-		    switch (operator) {
-		      case '+':
-		        result = first + second;
-		        break;
-
-		      case '-':
-		        result = first - second;
-		        break;
-
-		      case '*':
-		        result = first * second;
-		        break;
-
-		      case '/':
-		        result = first / second;
-		        break;
-
-
-		      default:
-		        System.out.printf("Error! operator is not correct");
-		        return;
-		    }
-
-		    System.out.println(first + " " + operator + " " + second + " = " + result);
-		  }
-
+		Odev1.alistirma01();
 		
+	} 
+		
+		
+	public static void alistirma01 () {
+		System.out.print("Hello \nOnur Ozkoca");
+	}
 	
 }
 
